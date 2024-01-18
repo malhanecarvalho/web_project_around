ac1130fc37001db886d22bdfc6d8f9565a65ef80
Malhane Carvalho <malhanec@gmail.com>
2024-01-18 07:59:32 +0000

    fix: corrige funcao de script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,12 @@
-let editButton = document.querySelector(".profile__button_icon_edit")
-let formElement =  document.querySelector(".popup")
-let closeButton = formElement.querySelector(".popup__icon")
-let nameInput = formElement.querySelector(".popup__description-name")
-let jobInput = formElement.querySelector(".popup__description-job")
-let saveInput = formElement.querySelector(".popup__button_text")
-let profileNameInput = document.querySelector(".profile__title")
-let profileJobInput = document.querySelector(".profile__subheading")
-let elementOverlay = document.querySelector(".content")
+const editButton = document.querySelector(".profile__button_icon_edit")
+const formElement =  document.querySelector(".popup")
+const closeButton = formElement.querySelector(".popup__icon")
+const nameInput = formElement.querySelector(".popup__description-name")
+const jobInput = formElement.querySelector(".popup__description-job")
+const saveInput = formElement.querySelector(".popup__button_text")
+const profileNameInput = document.querySelector(".profile__title")
+const profileJobInput = document.querySelector(".profile__subheading")
+const elementOverlay = document.querySelector(".content")
 
 function changeDisplayToFlex(){
 formElement.classList.add("popup-opened")
@@ -22,11 +22,15 @@ function changeDisplayToNone(){
   editButton.addEventListener("click", changeDisplayToFlex)
   closeButton.addEventListener("click", changeDisplayToNone)
 
+
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
+  profileNameInput.textContent = nameInput.value
+  profileJobInput.textContent = jobInput.value
   changeDisplayToNone()
 }
   formElement.addEventListener("submit", handleProfileFormSubmit)
+
 
 
 
