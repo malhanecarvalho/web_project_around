8d95c2009fc1704a9eb54f1da4e38f07f143eba4
Malhane Carvalho <malhanec@gmail.com>
2024-02-04 20:39:45 +0000

    feat: adiciona cartoes da pagina com script.js e suavidade aos popups

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,39 @@
 const editButton = document.querySelector(".profile__button_icon_edit")
-const formElement =  document.querySelector(".popup")
+const formElement = document.querySelector(".popup")
 const closeButton = formElement.querySelector(".popup__icon")
 const nameInput = formElement.querySelector(".popup__description-name")
 const jobInput = formElement.querySelector(".popup__description-job")
 const saveInput = formElement.querySelector(".popup__button_text")
 const profileNameInput = document.querySelector(".profile__title")
 const profileJobInput = document.querySelector(".profile__subheading")
-const elementOverlay = document.querySelector(".content")
+const addButton = document.querySelector(".profile__button-add_icon_add")
+const formElementAdd = document.querySelector(".popup-add")
+const closeButtonAdd = formElementAdd.querySelector(".popup-add__icon")
+const titleInput = formElementAdd.querySelector(".popup-add__description-title")
+const linkInput = formElementAdd.querySelector(".popup-add__description-link")
+const popupForm = document.querySelector(".popup-add__form")
+const createButton = formElementAdd.querySelector(".popup-add__button")
+const images = document.querySelectorAll(".cards__image")
+const titles = document.querySelectorAll(".cards__title")
+const imagePopupOnened = document.querySelector(".popup-img")
+const closeImage = document.querySelector(".popup-img")
+const allCards = document.querySelector(".cards").children
 
-function changeDisplayToFlex(){
-formElement.classList.add("popup-opened")
-nameInput.value = profileNameInput.textContent
-jobInput.value = profileJobInput.textContent
-elementOverlay.classList.add("content-overlay")
+
+
+//FUNÇÃO BOTÃO E EVENTO EDITAR PERFIL
+function changeDisplayToFlex() {
+  formElement.classList.add("popup-opened")
+  nameInput.value = profileNameInput.textContent
+  jobInput.value = profileJobInput.textContent
 }
 
-function changeDisplayToNone(){
+function changeDisplayToNone() {
   formElement.classList.remove("popup-opened")
 }
 
-  editButton.addEventListener("click", changeDisplayToFlex)
-  closeButton.addEventListener("click", changeDisplayToNone)
+editButton.addEventListener("click", changeDisplayToFlex)
+closeButton.addEventListener("click", changeDisplayToNone)
 
 
 function handleProfileFormSubmit(evt) {
@@ -29,9 +42,162 @@ function handleProfileFormSubmit(evt) {
   profileJobInput.textContent = jobInput.value
   changeDisplayToNone()
 }
-  formElement.addEventListener("submit", handleProfileFormSubmit)
+formElement.addEventListener("submit", handleProfileFormSubmit)
+
+// FUNÇÃO BOTÃO E EVENTO ADICIONAR
+
+function openDisplayToFlex() {
+  formElementAdd.classList.add("popup-add-opened")
+}
+
+function openDisplayToNone() {
+  formElementAdd.classList.remove("popup-add-opened")
+}
+
+addButton.addEventListener("click", openDisplayToFlex)
+closeButtonAdd.addEventListener("click", openDisplayToNone)
+
+
+function addButtonFormSubmit(evt, itens) {
+  evt.preventDefault();
+  console.log("alguem clicou")
+  addNewCard()
+  openDisplayToNone()
+  console.log(evt)
+}
+formElementAdd.addEventListener("submit", addButtonFormSubmit)
 
 
 
+
+// CARTÕES
+const container = document.querySelector(".cards")
+const iconAdd = document.querySelector(".profile__button-add")
+const iconClose = document.querySelector(".cards__icon-trash")
+
+const initialCards = [
+  {
+    name: "Vale de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg"
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg"
+  },
+  {
+    name: "Montanhas Carecas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg"
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg"
+  },
+  {
+    name: "Parque Nacional da Vanoise ",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg"
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg"
+  }
+];
+
+
+function addCard(card) {
+  const userTemplate = document.querySelector("#template").content.querySelector(".cards__container")
+  const cardElement = userTemplate.cloneNode(true);
+  cardElement.querySelector(".cards__title").textContent = card.name
+  cardElement.querySelector(".cards__image").setAttribute("src", card.link)
+  cardElement.querySelector(".cards__image").setAttribute("alt", card.name)
+  cardElement.querySelector(".cards__icon-trash").addEventListener("click", (evt) => {
+    evt.target.parentElement.remove()
+  })
+  cardElement.querySelector(".cards__icon-like").addEventListener("click", (evt) => {
+    evt.target.classList.toggle("cards__icon-like_active")
+
+  })
+  cardElement.querySelector(".cards__image").addEventListener("click", (evt)=>{
+    console.log("cliquei na imagem")
+    console.log(evt)
+    imagePopupOnened.classList.add("popup-img-opened")
+    const imagePopup = document.querySelector(".popup-img__photo")
+    const titlePopup = document.querySelector(".popup-img__title")
+    imagePopup.setAttribute("src", card.link)
+    imagePopup.setAttribute("alt", card.name)
+    titlePopup.textContent = card.name
+
+  })
+
+  return cardElement;
+}
+
+for (const card of initialCards) {
+  const cardItem = addCard(card)
+  container.append(cardItem)
+}
+
+//adicionar novo cartao
+function addNewCard() {
+  const title = document.querySelector(".popup-add__description-title")
+  const url = document.querySelector(".popup-add__description-link")
+
+  const cardItem = addCard({
+    name: title.value,
+    link: url.value
+  })
+
+  container.prepend(cardItem)
+  title.value = "";
+  url.value = "";
+}
+
+
+// fechar popup da imagem
+function closePopupImage() {
+  closeImage.classList.remove("popup-img-opened")
+}
+closeImage.addEventListener("click", closePopupImage)
+
+/*
+const imagePopup = document.querySelector(".popup-img__photo")
+images.forEach((image, index) => {
+  image.addEventListener("click", (evt) => {
+    imagePopupOnened.classList.add("popup-img-opened")
+   const src = evt.target.offsetParent.children[1].src
+   const alt = evt.target.offsetParent.children[1].alt
+    imagePopup.setAttribute("src", src )
+    imagePopup.setAttribute("alt", alt )
+  })
+
+})
+*/
+/*
+initialCards.forEach((card, index) => {
+  images[index].setAttribute("src", card.link)
+  images[index].setAttribute("alt", card.name)
+  titles[index].innerText = card.name
+
+})
+
+
+const buttonTrash = document.querySelectorAll("#button")
+buttonTrash.forEach((item, index) => {
+  item.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    evt.target.offsetParent.remove();
+
+  })
+  return item
+})
+
+const buttonLike = document.querySelectorAll("#button-like")
+buttonLike.forEach((item, index) => {
+  item.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    evt.target.classList.toggle("cards__icon-like_active")
+  })
+  return item
+})
+*/
 
 
